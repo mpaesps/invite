9632a05e153d4bfe77ae9f5f20f7eccbe8a7a6c2
Mayara Paes <mpaesps133@gmail.com>
2025-04-13 11:09:16 -0300

    ajustes finais

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Home from './pages/home/index.js';
+import Home from './pages/home/index.js'; // Certifique-se de que o caminho está correto
+import { BrowserRouter } from 'react-router-dom'; // Importando o BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter> {/* Envolva o componente Home com o BrowserRouter */}
+      <Home />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Se quiser medir o desempenho da sua aplicação, passe uma função para registrar os resultados
+// ou envie para um endpoint de análise.
 reportWebVitals();
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,11 @@
 import "./index.css";
 import Button from "@mui/material/Button";
+import { useSearchParams } from "react-router-dom";
 
 function Home() {
+  const [searchParams] = useSearchParams();
+  const nomeConvidado = searchParams.get("nome") || "Convidado";
+
   return (
     <div className="conteudo">
       <div className="caixa-mensagem">
@@ -10,16 +14,21 @@ function Home() {
           <br />
           Casa Nova
         </div>
+
         <div className="imagem-casa">
-          <img src="/assets/casa.png"></img>
+          <img src="/assets/casa.png" alt="Casa" />
         </div>
+
         <div className="subtitulo">Guilherme & Mayara</div>
+
         <div className="texto">
           <p>
-            Estamos de casa nova e queremos compartilhar essa alegria com você.
-            <br />Oii, & Família Sua presença fará desse momento algo ainda mais especial!
+            Oii, {nomeConvidado} & família! Estamos de casa nova e queremos compartilhar essa alegria com você.
+      
+             Sua presença fará desse momento algo ainda mais especial!
           </p>
         </div>
+
         <div className="data-hora-local">
           <div className="icon">
             <img
@@ -30,11 +39,11 @@ function Home() {
             31/05/2025
           </div>
           <div className="icon">
-            <img className="relogio" src="assets/clock.png"></img>
+            <img className="relogio" src="assets/clock.png" alt="Relógio" />
             15:00
           </div>
           <div className="icon">
-            <img className="pin" src="assets/pin.png"></img>
+            <img className="pin" src="assets/pin.png" alt="Localização" />
             <a
               href="https://maps.app.goo.gl/KVNSWMV5q2TKev7p8"
               target="_blank"
@@ -44,9 +53,11 @@ function Home() {
             </a>
           </div>
         </div>
+
         <div className="bot">
-          <Button variant="contained" className="botao-bege-rosado">
-           Escolher Presente   <img className="presente" src="assets/presente.png"></img>     
+          <Button variant="contained" className="botao-bege-rosado" href="https://forms.gle/fMdyQArJAx7UJbfB6">
+            Escolher Presente{" "}
+            <img className="presente" src="assets/presente.png" alt="Presente" />
           </Button>
         </div>
       </div>
